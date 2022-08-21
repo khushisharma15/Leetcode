5eedcb3d562e3edf82ac1494664b67fa6b81670c
khushisharma15 <91364198+khushisharma15@users.noreply.github.com>
2022-08-21 13:49:26 +0530

    Time: 0 ms (100.00%), Space: 44.4 MB (10.98%) - LeetHub

---
+++ 108-convert-sorted-array-to-binary-search-tree/108-convert-sorted-array-to-binary-search-tree.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        if(nums.length == 0)
+                    return null;
+
+            return sortedArrayToBST(nums, 0, nums.length - 1);
+        }
+
+        public static TreeNode sortedArrayToBST(int[] nums, int start, int end){
+            if(end < start)
+                return null;
+
+            int mid = (start + end) / 2;
+
+            TreeNode node = new TreeNode(nums[mid]);
+
+            node.left = sortedArrayToBST(nums, start, mid - 1);
+            node.right = sortedArrayToBST(nums, mid+1, end);
+
+            return node;
+        }
+}
