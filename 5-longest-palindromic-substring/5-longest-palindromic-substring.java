7b410537edc0d66ad6e4875619c754fb91cff821
khushisharma15 <91364198+khushisharma15@users.noreply.github.com>
2022-08-26 13:12:03 +0530

    Time: 43 ms (68.90%), Space: 52.5 MB (39.86%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.java
@@ -5,18 +5,18 @@ class Solution {
             String s1=palindrome(s,i,i);
             String s2=palindrome(s,i,i+1);
             
-            res = res.length() >s1.length()?res:s1;
-            res = res.length()>s2.length()?res:s2;
+            res= res.length()>s1.length()?res:s1;
+            res= res.length()>s2.length()?res:s2;
         }
         return res;
         
     }
     
     public String palindrome(String s, int left, int right){
-        while(left>=0 && right<s.length() && s.charAt(left)==s.charAt(right)){
-            left--;right++;
+        while(left>=0 &&right<s.length() && s.charAt(left)==s.charAt(right)){
+            left--;
+            right++;
         }
         return s.substring(left+1,right);
     }
 }
-
